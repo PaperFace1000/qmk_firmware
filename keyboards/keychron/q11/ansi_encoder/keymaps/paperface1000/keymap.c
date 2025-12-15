eb27dd60c78c4268ef2529a95cfaebc8acc97c8e
paperface1000 <paperface1000eng@gmail.com>
2025-12-15 21:01:59 +0900

    :+1: キーボードの言語設定が英語扱いだと変換・無変換が無視されるのでF13とF14を割り当てた

--- keyboards/keychron/q11/ansi_encoder/keymaps/paperface1000/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/paperface1000/keymap.c
@@ -35,7 +35,7 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
+        KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_HOME,
         MC_1,     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         MC_2,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         MC_3,     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MC_2,     KC_TAB,            KC_Q,     KC_W,                    KC_E,             KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         MC_3,     MO(WIN_FN),        KC_A,     KC_S,                    KC_D,             KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         MC_4,     KC_LSFT,                     KC_Z,                    KC_X,             KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        MC_5,     KC_ESC,            KC_LWIN,  KC_LALT,                 LCTL_T(KC_F13),                 KC_SPC,                        KC_SPC,        RCTL_T(KC_F14),  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        MC_5,     KC_LCTL,           KC_LALT,  LCTL_T(KC_F13),          KC_LWIN,          KC_SPC,                        KC_SPC,        RCTL_T(KC_F14),  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_91_ansi(
         _______,  _______,     _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,     _______,     _______,     _______,     _______,  _______,     _______,     _______,
